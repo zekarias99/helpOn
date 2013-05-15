e7cf9037633feb62229331789e3db432771cb4c1
Zekarias Araya <zekarias99@gmail.com>
2013-05-15 13:22:31 -0700

    Finish static-pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,13 @@ describe "Static pages" do
       expect(page).to have_content('Helpon')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Helpon | Home")
+      expect(page).to have_title("Helpon")
+    end
+    it "It should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
