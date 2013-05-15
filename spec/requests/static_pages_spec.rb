ffe49fe11fb82fc0f539d5dadfce0965c6f8acbd
Zekarias Araya <zekarias99@gmail.com>
2013-05-15 16:34:45 -0700

    Add css, partials and tests for the named routes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,12 +22,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Helpon | Help")
     end
   end
@@ -35,12 +35,12 @@ describe "Static pages" do
   describe "About Us page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Helpon | About Us")
     end
   end
@@ -48,12 +48,12 @@ describe "Static pages" do
   describe "How It Work's page" do
 
     it "should have the content 'How It Work's'" do
-      visit '/static_pages/how_it_works'
+      visit how_it_works_path
       expect(page).to have_content("How It Work's")
     end
 
     it "should have the title 'How It Work'" do
-      visit '/static_pages/how_it_works'
+      visit how_it_works_path
       expect(page).to have_title("Helpon | How It Works")
     end
   end
@@ -61,12 +61,12 @@ describe "Static pages" do
   describe "Contact Us page" do
 
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content("Contact Us")
     end
 
     it "should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Helpon | Contact Us")
     end
   end
