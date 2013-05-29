e359ee67607eaeab8f684d72ace6c90001a873a5
Zekarias Araya <zekarias99@gmail.com>
2013-05-29 09:37:19 -0700

    Add some spec to highlight model

--- spec/models/highlight_spec.rb
+++ spec/models/highlight_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Highlight do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @highlights = Highlight.new(highlight_attributes) }
+
+  subject { @highlights }
+
+  it {should respond_to (:description)}
+  it {should respond_to (:deal_id)}
+
+  describe "when fine_print is not present" do
+    before { @highlights.description = " " }
+    it { should_not be_valid }
+  end
 end
