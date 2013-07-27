f1d419946502a01c6b1ff9864179d17009c27a68
Zekarias Araya <zekarias99@gmail.com>
2013-07-26 17:29:07 -0700

    Update the so-called secret token used by Rails to encrypt session varibles so that it is dynamically generated rather than hard-coded.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Helpon::Application.config.secret_key_base = '83009e15eb0085bb3357fea2e3f623d20f6e8d020b571c934f4cb5c3c872dc6af0eeb664916f71376576bf2cf114ad81ccf96dd9cff8b0c45dc51e6e0a4e20cb'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+Helpon::Application.config.secret_key_base = secure_token
