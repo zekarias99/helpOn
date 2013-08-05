7a68a2cdf19c2f2eb5ee827bfbf74a5df7599e24
Zekarias Araya <zekarias99@gmail.com>
2013-08-04 17:08:24 -0700

    Update to Rails 4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -125,7 +125,7 @@ describe User do
     describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit edit_user_path(user) }
-
+    
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title("Edit user") }
